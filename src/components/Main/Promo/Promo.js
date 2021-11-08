988af82def963b4c4d90f45b6601826c5578f068
Ирина Пупкова <pupkova.ia@yandex.ru>
2021-11-08 17:31:04 +0500

    feat: добавить плавный переход по кнÐ яклорной ссылке

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from '../../../images/promo-logo.svg';
 import './Promo.css';
 
 function Promo() {
+
+
+  useEffect(() => {
+    const btn = document.querySelector('.promo__btn');
+
+    btn.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      const blockID = btn.getAttribute('href').substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    });
+  }, [])
+
   return (
     <section className="promo">
       <div className="promo__container">
