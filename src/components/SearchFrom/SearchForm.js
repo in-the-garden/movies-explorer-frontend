8238b3194826d3afc5ae068d2de4ec2d17ea7a40
Ирина Пупкова <pupkova.ia@yandex.ru>
2021-11-15 23:00:38 +0500

    feat: зазакончить верстку компонента для десктопной версии

--- src/components/SearchFrom/SearchForm.js
+++ src/components/SearchFrom/SearchForm.js
@@ -7,9 +7,13 @@ function SearchForm() {
   <form className="search-form">
     <div className="search-form__container">
       <input className="search-form__input" placeholder="Фильм" />
-      <button className="search-form__btn" type="submit"></button>
+      <button className="search-form__btn" type="submit" onClick={(evt) => evt.preventDefault()}></button>
     </div>
-    <div className="search-form__type">
+    <div className="search-form__filter-container">
+      <div className="search-form__btns-filter">
+        <button className="search-form__btn-filter" type="button" onClick={(evt) => evt.preventDefault()}></button>
+        <button className="search-form__btn-filter visible" type="button" onClick={(evt) => evt.preventDefault()}></button>
+      </div>
       Короткометражки
     </div>
   </form>
